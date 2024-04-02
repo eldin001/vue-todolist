3c8e5ddf452d388d9e0a087c38c306c14943e49e
eldin001 <eldin.balje@icloud.com>
2024-04-02 17:04:39 +0200

    minor fix

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ createApp({
     data(){
         return{
             topics: topics,
+            itemText: '',
         }
     },
     methods: {
@@ -16,6 +17,29 @@ createApp({
             if(element) {
             element.done = !element.done;
         }
+        },
+        removeItem(id) {
+            const i = this.topics.findIndex((el)=> el.id === id);
+            if(i !== -1) {
+                this.topics.splice(i,1);
+            }
+            
+        },
+        addTodo() {
+            const newObj = {
+                id: null,
+                text: this.itemText,
+                done: false
+            }
+            let nextId = 0;
+            this.topics.forEach((el)=>{
+                if(nextId < el.id) {
+                    nextId = el.id;
+                }
+            });
+            newObj.id = nextId +1;
+            this.topics.push(newObj)
+            
         }
     }
 }).mount('#app')
